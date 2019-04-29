dba480c6c8fef416f40cc0d458d6b9f7297ca032
joshua-shequin-9147 <joshua.shequin.9147@mail.linnbenton.edu>
2019-04-29 11:15:35 -0700

    Adding the skeleton module for our gui_elements that our Frame module will use.

---
+++ gui_elements.py
@@ -0,0 +1,11 @@
+"""
+Contains GUI elements that we will use within our Frame object.
+
+Contains things like buttons, drop-downs, etc. that we will use within our Frame object.
+Placing them all within this module just for organization sakes and ease of use.
+"""
+
+
+class Button:
+
+    pass
